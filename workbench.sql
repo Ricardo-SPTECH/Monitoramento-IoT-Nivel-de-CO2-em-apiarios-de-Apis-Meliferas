defa5adb613a426b007e4aed4d205c419edd698d
Ricardo.SPTECH <ricardo.inoue@sptech.school>
2026-03-08 17:55:01 -0300

    mais Inserts e Selects - Ricardo e Yasmin

--- workbench.sql
+++ workbench.sql
@@ -10,22 +10,71 @@ Senha varchar (12)
 
 create table monitoramento (
 Apecuario int not null primary key,
-CO2 int not null,
+CO2_Porcentagem int not null, 
 Nível_CO2 varchar (50) not null,
 constraint chkCO2 check (Nível_CO2 in ('baixo', 'médio','crítico'))
 );
 
-insert into Login (Nome,Email,Senha)
+nsert into Login (Nome,Email,Senha)
 values ('Yasmin','yasmin.nascimento@sptech.school','Urubu100'),
-('Maria','Maria.Bispo@sptech.school','Urubu100');
+('Maria','maria.bispo@sptech.school','Urubu100'),
+('Thabata Vitoria','thabata.sousa@sptech.school','Urubu100'),
+ ('Maria Clara Minomo','maria.azevedo@sptech.school','Urubu100'),
+('Marcelo Carlos','Maria.carvalho@sptech.school','Urubu100'),
+('Ricardo','ricardo.inoue@sptech.school', 'Urubu100');
 
-insert into monitoramento (Apecuario,CO2,Nível_CO2)
-values (1,1,'baixo');
+insert into monitoramento (Apecuario,CO2_Porcentagem,Nível_CO2)
+values (1,9,'baixo'),
+(2,18, 'médio'),
+(3,25, 'crítico'),
+(4,21, 'médio'),
+(5,26, 'crítico');
+
 
 select * from Login;
 
-
 select * from monitoramento;
+
+select nome, email from Login;
+
+select Apecuario, Nível_CO2 from monitoramento;
+
+select * from login
+where nome like '%B%';
+
+select * from login 
+where nome like 'M%';
+
+select * from login 
+where nome like '%S';
+
+select * from login 
+where nome like 'R%';
+
+select * from login 
+where nome not like '%A%';
+
+select * from monitoramento
+where Nível_CO2 = 'crítico';
+
+select * from monitoramento
+where Nível_CO2 = 'médio';
+
+select * from monitoramento
+where Nível_CO2 = 'baixo';
+
+select email,
+concat (' O usuário é ', nome,  ' e a senha é: ', senha) as Cadastro
+from Login;
+
+select *,
+case when Nível_CO2 = 'crítico' 
+then concat ('A colmeia ', Apecuario, ' está em nível ', Nível_CO2, ' e exige atenção imediata ' ) 
+when Nível_CO2 = 'médio' 
+then concat ('A colmeia ', Apecuario, ' está em nível ', Nível_CO2, ' verifique a colmeia ')
+else ('Essa Colmeia está controlada')
+end  as nível_atenção
+from monitoramento;
 
 alter table monitoramento
 modify column Apecuario int not null;
@@ -37,10 +86,6 @@ update monitoramento
 set id = 1
 where apecuario = 1;
 
-insert into monitoramento (id,Apecuario,CO2,Nível_CO2)
-values (2,1,1,'crítico');
-
-select * from monitoramento;
 
 
 
